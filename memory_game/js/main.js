b5685b041cf670a275310308ec4b5990e18f6d90
Sean Le <seanle0405@hotmail.com>
2019-03-12 04:23:32 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,13 +31,26 @@ var checkForMatch = function(){
 		}
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png" );
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+};
+
+
+createBoard();
